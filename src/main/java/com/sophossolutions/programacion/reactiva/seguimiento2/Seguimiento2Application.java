82a6be9582e5da14573c78ff8e13b99620b5eb8e
cbello <cbello@bancolombia.com.co>
2023-08-10 21:44:16 -0500

    create project structure

---
+++ src/main/java/com/sophossolutions/programacion/reactiva/seguimiento2/Seguimiento2Application.java
@@ -0,0 +1,13 @@
+package com.sophossolutions.programacion.reactiva.seguimiento2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Seguimiento2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Seguimiento2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sophossolutions/programacion/reactiva/seguimiento2/Seguimiento2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sophossolutions.programacion.reactiva.seguimiento2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Seguimiento2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
